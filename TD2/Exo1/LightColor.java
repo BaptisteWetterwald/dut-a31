f66e410286c18a615d21f2f7a885e467e77fd864
Baptiste Wetterwald <baptiste.wetterwald@etu.unistra.fr>
2021-09-15 15:24:35 +0200

    TD2

---
+++ TD2/Exo1/LightColor.java
@@ -0,0 +1,5 @@
+package TD1.Exo3;
+
+public enum LightColor {
+    GREEN, ORANGE, RED;
+}
---
+++ TD2/Exo1/LightStrategy.java
@@ -0,0 +1,6 @@
+package TD1.Exo3;
+
+public interface LightStrategy
+{
+    LightColor getNextColor(TrafficLight trafficLight);
+}
