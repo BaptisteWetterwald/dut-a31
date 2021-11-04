e938823e61f2a2c620ba44712a0fbed004867c46
Baptiste Wetterwald <baptiste.wetterwald@etu.unistra.fr>
2021-11-04 16:58:51 +0100

    Update TP note

--- src/TP_Note/v3/controller/Main.java
+++ src/TP_Note/v3/controller/Main.java
@@ -1,12 +1,14 @@
 package TP_Note.v3.controller;
 
 import TP_Note.v3.controller.CookieFactory;
+import TP_Note.v3.model.CookieLemon;
 import TP_Note.v3.view.CookieObserver;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        CookieObserver view = new CookieObserver();
+        PaquetCookies paquet = new PaquetCookies();
+        paquet.addObserver(new CookieObserver(new CookieLemon()));
     }
 }
